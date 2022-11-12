fc26b9be2d64b4ee0ede5578234b7b0eeb79ba40
junxian428 <holeyiho@gmail.com>
2022-11-12 22:58:22 +0800

    first commit

---
+++ src/main/java/com/example/spring/service/EmployeeService.java
@@ -0,0 +1,16 @@
+package com.example.spring.service;
+
+import java.util.List;
+
+import org.springframework.data.domain.Page;
+
+import com.example.spring.model.Employee;
+
+
+public interface EmployeeService {
+    List<Employee> getAllEmployees();
+    void saveEmployee(Employee employee);
+    Employee getEmployeeById(long id);
+	void deleteEmployeeById(long id);
+	Page<Employee> findPaginated(int pageNo, int pageSize, String sortField, String sortDirection);
+}
---
+++ src/main/java/com/example/spring/service/EmployeeServiceIpl.java
@@ -0,0 +1,67 @@
+package com.example.spring.service;
+
+import java.util.List;
+import java.util.Optional;
+
+import javax.management.RuntimeErrorException;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.stereotype.Service;
+
+import com.example.spring.model.Employee;
+import com.example.spring.repository.EmployeeRepository;
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.stereotype.Service;
+
+@Service
+public class EmployeeServiceIpl implements EmployeeService{
+
+    @Autowired
+    private EmployeeRepository employeeRepository;
+    @Override
+    public List<Employee> getAllEmployees() {
+        // TODO Auto-generated method stub
+        return employeeRepository.findAll();
+    }
+    @Override
+    public void saveEmployee(Employee employee) {
+        // TODO Auto-generated method stub
+        this.employeeRepository.save(employee);
+        
+    }
+    @Override
+    public Employee getEmployeeById(long id) {
+        // TODO Auto-generated method stub
+        Optional<Employee> optional = employeeRepository.findById(id);
+        Employee employee = null;
+        if(optional.isPresent()){
+            employee = optional.get();
+        } else{
+            throw new RuntimeException("Employee New Found for ID" + id);
+        }
+        return employee;
+    }
+    @Override
+    public Page<Employee> findPaginated(int pageNo, int pageSize, String sortField, String sortDirection) {
+        // TODO Auto-generated method stub
+        Sort sort = sortDirection.equalsIgnoreCase(Sort.Direction.ASC.name()) ? Sort.by(sortField).ascending() :
+			Sort.by(sortField).descending();
+		
+		Pageable pageable = PageRequest.of(pageNo - 1, pageSize, sort);
+		return this.employeeRepository.findAll(pageable);
+    }
+    @Override
+    public void deleteEmployeeById(long id) {
+        // TODO Auto-generated method stub
+        this.employeeRepository.deleteById(id);
+    }
+    
+}
